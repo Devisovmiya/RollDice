0edd682c1d895417c5e6f175e9493ab7ccbd77ca
Devisovmiya <sovmiya2000@gmail.com>
2025-09-01 07:58:56 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import "package:flutter_application_1/gradient_container.dart";
+
+void main(){
+  runApp(
+    MaterialApp(
+      home:Scaffold(
+        body: GradientContainer(Colors.indigoAccent,
+      Colors.lightBlueAccent,
+      )
+        ),
+        ),
+      );
+}
+
+
+
